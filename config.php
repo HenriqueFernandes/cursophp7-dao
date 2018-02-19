a2f7445a220c9dcbda59301a926efcf3a354f4e4
HenriqueFernandes <henrique.fernandes.h@gmail.com>
2018-02-19 15:16:01 -0300

    data

--- config.php
+++ config.php
@@ -3,10 +3,10 @@
 spl_autoload_register(function($class_name){
 	$fileName = "class".DIRECTORY_SEPARATOR.$class_name.".php";
 	if(file_exists(($fileName))){
-		echo $fileName;
+		
 		require_once($fileName);
 	}
-echo $fileName;
+
 });
 
  ?>
